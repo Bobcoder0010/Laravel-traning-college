8059c5d62a42d22c2afcb50638b4871ccdcaf677
Aashishfedora <dellizulter.com>
2024-08-11 08:37:14 +0545

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<h1>Hello world display hu haai</h1>
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Bobcoder0010/Laravel-traning-college.git
+git push -u origin main
